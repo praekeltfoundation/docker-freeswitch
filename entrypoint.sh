8bfabb1831055317aa055199df931b1dfb64234e
Jamie Hewland <jhewland@gmail.com>
2017-03-07 16:22:18 +0200

    Prevent freeswitch stdout buffering when not in a terminal

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,10 @@ if [ "${1:0:1}" = '-' ]; then
   set -- freeswitch "$@"
 fi
 
+# If we're running FreeSWITCH and are not in a terminal, then prevent FreeSWITCH
+# from buffering I/O for its CLI.
+if [ "$1" = 'freeswitch' ] && ! [ -t 1 ]; then
+  set -- stdbuf -i0 -o0 -e0 "$@"
+fi
+
 exec "$@"
